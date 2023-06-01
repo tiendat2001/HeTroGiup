e96e1486fb7b77d1e9b0f4c1a4b6a03167d6a4ff
tiendat2001 <75568539+tiendat2001@users.noreply.github.com>
2023-06-01 12:42:48 +0700

    1

---
+++ demo/src/main/java/com/example/demo/UngVien.java
@@ -0,0 +1,38 @@
+package com.example.demo;
+
+public class UngVien {
+    public String maUngVien;
+    public String hoTen;
+
+    public UngVien(String maUngVien, String hoTen) {
+        this.maUngVien = maUngVien;
+        this.hoTen = hoTen;
+    }
+
+    public String getMaUngVien() {
+        return maUngVien;
+    }
+
+    public void setMaUngVien(String maUngVien) {
+        this.maUngVien = maUngVien;
+    }
+
+    public String getHoTen() {
+        return hoTen;
+    }
+
+    public void setHoTen(String hoTen) {
+        this.hoTen = hoTen;
+    }
+
+    public int getNamsinh() {
+        return namsinh;
+    }
+
+    public void setNamsinh(int namsinh) {
+        this.namsinh = namsinh;
+    }
+
+    public int namsinh;
+
+}
---
+++ demo/src/main/java/com/example/demo/UngVienController.java
@@ -0,0 +1,57 @@
+package com.example.demo;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+
+import java.net.URL;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ResourceBundle;
+
+public class UngVienController implements Initializable {
+    public static ObservableList<UngVien> list = FXCollections.observableArrayList();
+
+
+    @FXML
+    private TableView<UngVien> tbl_UngVien;
+
+    @FXML
+    private TableColumn<?, ?> col_hoten;
+
+    @FXML
+    private TableColumn<?, ?> col_maUngVien;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        // Thông tin kết nối cơ sở dữ liệu
+        String urlDB = "jdbc:mysql://localhost:3306/hetrogiup";
+        String username = "root";
+        String password = "";
+
+        try {
+            Connection conn = DriverManager.getConnection(urlDB, username, password);
+            String sql = "Select * from ungvien ";
+            Statement Statement = conn.createStatement();
+            ResultSet resultSet = Statement.executeQuery(sql);
+            while (resultSet.next()) {
+                list.add(new UngVien(
+                        "dat","dattt")
+
+                );
+
+            }
+//            col_maUngVien.setCellValueFactory(new PropertyValueFactory<>("maUngVien"));
+//            col_hoten.setCellValueFactory(new PropertyValueFactory<>("hoTen"));
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+    }
+}
